6ae7c0e887bd25f26dfbd19383f516200dfdcdac
Prabal Banerjee <prabal.banerjee@polygon.technology>
2022-08-26 17:59:35 +0400

    add: server constraint check + client header fetch

--- server/client.js
+++ server/client.js
@@ -1,21 +1,25 @@
 const axios = require('axios')
-const crypto = require("crypto")
+const crypto = require('crypto')
+const fs = require('fs')
+const api = require('@polkadot/api')
 
 const buildEddsa = require("circomlibjs").buildEddsa
 const buildBabyjub = require("circomlibjs").buildBabyjub
 const buildPoseidon = require("circomlibjs").buildPoseidon
 
-async function doPostRequest() {
+async function doPostRequest(msg, number) {
   eddsa = await buildEddsa()
   babyJub = await buildBabyjub()
   poseidon = await buildPoseidon()
   F = poseidon.F
 
-  var msg = poseidon([0])
-  var prvKey = crypto.randomBytes(32)
+  // var msg = poseidon([0])
+  // var prvKey = crypto.randomBytes(32)
+  var prvKey = fs.readFileSync('keystore')
+  prvKey = new Uint8Array(Buffer.from(JSON.parse("[" + prvKey + "]")))
   var pubKey = eddsa.prv2pub(prvKey)
   var signature = eddsa.signPoseidon(prvKey, msg)
-  // console.log(msg, signature, pubKey)
+  console.log(msg, signature, pubKey)
 
   const res = await axios.post('http://localhost:8080/sign', {
     signature: signature.S.toString(),
@@ -24,17 +28,86 @@ async function doPostRequest() {
     message: msg.toString(),
     pubkeyX: pubKey[0].toString(),
     pubkeyY: pubKey[1].toString(),
+    blockNumber: number
   });
 
   let data = res.data
   console.log(data)
 }
 
-doPostRequest()
-doPostRequest()
-doPostRequest()
-doPostRequest()
-doPostRequest()
-doPostRequest()
-doPostRequest()
-doPostRequest()
+// doPostRequest()
+
+async function main () {
+  const api = await createApi('ws://127.0.0.1:9944')
+
+  const unsubscribe = await api.rpc.chain.subscribeFinalizedHeads((header) => {
+    console.log(`Chain is at finalized block: #${header.number}`);
+
+    var dataRoot = header.extrinsicsRoot.dataRoot
+    dataRoot = new Uint8Array(Buffer.from(dataRoot))
+    doPostRequest(dataRoot, header.number)
+  });
+
+}
+
+main()
+
+// Helpers
+
+async function createApi(url) {
+    const provider = new api.WsProvider(url)
+
+    // Create the API and wait until ready
+    return api.ApiPromise.create({
+        provider,
+        rpc: {
+            kate: {
+                queryDataProof: {
+                    description: 'Fetch proofs of data inclusion inside data root',
+                    params: [
+                        {
+                            name: 'blockNumber',
+                            type: 'u64'
+                        },
+                        {
+                            name: 'txIndex',
+                            type: 'u64',
+                        }
+                    ],
+                    type: 'Vec<[u8;32]>'
+                },
+            },
+        },
+        types: {
+            DataLookup: {
+                size: 'u32',
+                index: 'Vec<(u32,u32)>'
+            },
+            KateExtrinsicRoot: {
+                hash: 'Hash',
+                commitment: 'Vec<u8>',
+                rows: 'u16',
+                cols: 'u16',
+                dataRoot: 'Hash'
+            },
+            KateHeader: {
+                parentHash: 'Hash',
+                number: 'Compact<BlockNumber>',
+                stateRoot: 'Hash',
+                extrinsicsRoot: 'KateExtrinsicRoot',
+                digest: 'Digest',
+                appDataLookup: 'DataLookup'
+            },
+            Header: 'KateHeader',
+            AppId: 'u32',
+        },
+        signedExtensions: {
+            CheckAppId: {
+                extrinsic: {
+                    appId: 'u32'
+                },
+                payload: {}
+            },
+        },
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const buildEddsa = require("circomlibjs").buildEddsa
+const wasm_tester = require("circom_tester").wasm
+const buildBabyjub = require("circomlibjs").buildBabyjub
+const buildPoseidon = require("circomlibjs").buildPoseidon
 
 const app = express()
 app.use(express.json()) 
@@ -22,24 +25,27 @@ app.post('/sign', async function(req, res) {
       new Uint8Array(Buffer.from(JSON.parse("[" + req.body.pubkeyY + "]")))
     ]
     var msg = new Uint8Array(Buffer.from(JSON.parse("[" + req.body.message + "]")))
+    var blockNumber = req.body.blockNumber
     // console.log(msg, sig, pubkey)
     if (!eddsa.verifyPoseidon(msg, sig, pubkey)) {
       throw new Error('Signature cannot be verified!')
     }
-    console.log('Signature verified message received!')
+    console.log('Signature verified message received for block number ', req.body.blockNumber)
 
     signStruct = {
+      msg: req.body.message,
       pubkey: [req.body.pubkeyX, req.body.pubkeyY],
-      signature: req.body.signature
+      S: req.body.signature,
+      R8: [req.body.R8x, req.body.R8y]
     }
 
     // TODO: Protect against data races
-    if (msg in inMemoryDB) {
-      var val = inMemoryDB[msg]
-      val += signStruct
-      inMemoryDB[msg] = val
+    if (blockNumber in inMemoryDB) {
+      var val = inMemoryDB[blockNumber]
+      val.push(signStruct)
+      inMemoryDB[blockNumber] = val
     } else {
-      inMemoryDB[msg] = [signStruct]
+      inMemoryDB[blockNumber] = [signStruct]
     }
 
     res.send('Signature verified and stored successfully!')
@@ -48,14 +54,87 @@ app.post('/sign', async function(req, res) {
     console.log('Error occurred!\n', e)
     res.send(e)
   }
-
-
-
 })
 
 app.get('/', function(req, res) {
   res.json(inMemoryDB)
 })
 
+app.get('/proof', async function(req, res) {
+  var blockNumber = req.query.blockNumber
+  if (blockNumber === undefined || !(blockNumber in inMemoryDB)) {
+    res.send('Bad block number')
+    return
+  }
+  
+  eddsa = await buildEddsa()
+  babyJub = await buildBabyjub()
+  poseidon = await buildPoseidon()
+  F = poseidon.F
+  circuit = await wasm_tester('../circuits/attestation.circom')
+
+  var validatorSet = []
+  var validatorsR8x = []
+  var validatorsR8y = []
+  var validatorsS = []
+  var validatorsIsSigned = []
+  var dataRoot = new Uint8Array(Buffer.from(JSON.parse("[" + inMemoryDB[blockNumber][0].msg + "]")))
+    
+  for (var i=0; i<5; i++) {
+
+    // If part of valdator set
+    if (i < inMemoryDB[blockNumber].length) {
+      var pubkey = [
+        new Uint8Array(Buffer.from(JSON.parse("[" + inMemoryDB[blockNumber][i].pubkey[0] + "]"))),
+        new Uint8Array(Buffer.from(JSON.parse("[" + inMemoryDB[blockNumber][i].pubkey[1] + "]")))
+      ]
+      var R8 = [
+        new Uint8Array(Buffer.from(JSON.parse("[" + inMemoryDB[blockNumber][i].R8[0] + "]"))),
+        new Uint8Array(Buffer.from(JSON.parse("[" + inMemoryDB[blockNumber][i].R8[1] + "]")))
+      ]
+      var sig = BigInt(inMemoryDB[blockNumber][i].S)
+      var weight = 1  //TODO: fetch from on-disk set
+
+      validatorSet.push(F.toObject(pubkey[0]))
+      validatorSet.push(F.toObject(pubkey[1]))
+      validatorSet.push(F.toObject(weight))
+
+      validatorsR8x.push(F.toObject(R8[0]))
+      validatorsR8y.push(F.toObject(R8[1]))
+      validatorsS.push(sig)
+
+      validatorsIsSigned.push(1)
+
+    } else {
+      var weight = 1  //TODO: fetch from on-disk set
+
+      validatorSet.push(F.toObject())
+      validatorSet.push(F.toObject())
+      validatorSet.push(F.toObject(weight))
+
+      validatorsR8x.push(F.toObject())
+      validatorsR8y.push(F.toObject())
+      validatorsS.push(0)
+
+      validatorsIsSigned.push(0)
+    }
+
+  }
+
+  const w = await circuit.calculateWitness({
+    validatorSet: validatorSet, 
+    dataRoot: F.toObject(dataRoot),
+    validatorsR8x: validatorsR8x,
+    validatorsR8y: validatorsR8y,
+    validatorsS: validatorsS,
+    validatorsIsSigned: validatorsIsSigned
+  }, true)
+
+  await circuit.checkConstraints(w)
+
+  res.send('Constraints checked!')
+})
+
 var inMemoryDB = {}
 app.listen(8080)
+
